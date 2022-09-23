bb7d45457d58edfef90708c945e25a11d564abda
davidsretzlaff <davids.retzlaff@gmail.com>
2022-09-23 11:55:59 -0300

    interfaces and class

--- class.ts
+++ class.ts
@@ -1,23 +1,39 @@
-class Animal {
-    /*
-    name:string;
+interface AnimalInterface{
+    name: string;
     color:string;
 
-    // bad away
-    constructor(name:string, color:string){
-        this.name = name;
-        this.color = color;
-    }
-    */
-    
-    // good away
+    run()
+    eat()
+}
+class Animal implements AnimalInterface{
+    // create constructor and create attributes of class
     constructor(public name:string, public color:string){
-        
+    }
+
+    //function
+    run(){
+        console.log('running');
+    }
+    eat(){
+        console.log('Eat')
     }
 }
 
-let myAnimal = new Animal('Dog','black');
+//using herança
+class Dog extends Animal{
+    breed: string;
+
+    //overriding the eat method
+    eat(){
+        //super.eat call eat method from animal
+        super.eat();
+
+        console.log('Eat how dog')
+    }
+}
+
+let bud : Dog = new Dog('Bud','black');
+bud.Run();
 
 // here i am saying that the variable is type Animal
 let myAnimalType: Animal = new Animal('Dog','black');
-myAnimal.name;
